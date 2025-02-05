d9bf5090195a3543369f6e333eb8a6b69dfad68c
s111ew <167631312+s111ew@users.noreply.github.com>
2025-02-05 20:01:05 +0000

    Add styling and snap scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,80 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import arrowSvg from './assets/arrow.svg'
+import sparkleSvg from './assets/sparkle.svg'
 
 function App() {
-  const [article, setArticle] = useState(null);
+  const [articles, setArticles] = useState([]);
   const [hasLoaded, setHasLoaded] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    fetch("https://en.wikipedia.org/api/rest_v1/page/random/summary")
+    const fetchArticles = async () => {
+      const requests = Array.from({length: 5}, fetchArticle);
+      const returnedArticles = await Promise.all(requests);
+      setArticles(returnedArticles);
+      setHasLoaded(true)
+    };
+    fetchArticles();
+  }, []);
+
+  const fetchArticle = () => {
+    return fetch("https://en.wikipedia.org/api/rest_v1/page/random/summary")
       .then((response) => response.json())
       .then((data) => {
-        setArticle({
+        return {
           imageUrl: data.originalimage.source || "",
           title: data.title,
-          description: data.extract,
+          description: data.extract.split(' ').length > 25 ? `${data.extract.split(' ').slice(0, 25).join(' ')}...` : data.extract,
           link: data.content_urls.desktop.page,
-        });
-        setHasLoaded(true);
+        };
       })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  }
 
   return (
     <>
-      {hasLoaded && article ? <Card article={article}/> : <p>Loading...</p>}
+      <Header isLoggedIn={isLoggedIn} />
+      {hasLoaded && articles.length > 0 ? <Cards isLoggedIn={isLoggedIn} articles={articles}/> : <p>Loading...</p>}
     </>
   )
 }
 
-function Card({ article }) {
+function Cards({ articles }) {
+  const articlesToRender = articles.map(article =>
+    <div key={article.title} className="card" style={{backgroundImage: `url(${article.imageUrl})`}}>
+      <div className='card-text'>
+        <h2 className="card-title">{article.title}</h2>
+        <p className="card-description">{article.description}</p>
+        <div className='card-buttons'>
+          <a className="card-link" href={article.link} target="_blank" rel="noopener noreferrer">
+            Article<img className='arrow-svg' src={arrowSvg}></img>
+          </a>
+          <AIbutton title={article.title}/>
+        </div>
+      </div>
+    </div>
+  )
   return(
-    <div className="card">
-      {article.imageUrl && <img className='card-image' src={article.imageUrl} alt={article.title} />}
-      <h2 className="card-title">{article.title}</h2>
-      <p className="card-description">{article.description}</p>
-      <a className="card-link" href={article.link} target="_blank" rel="noopener noreferrer">
-        Read More
-      </a>
+    <div className='card-stream'>
+      {articlesToRender}
+    </div>
+  )
+}
+
+function AIbutton({ title }) {
+  return(
+    <div className='card-ai-button'>
+      <span>AI Further Reading</span>
+      <img className='sparkle-svg' src={sparkleSvg}/>
+    </div>
+  )
+}
+
+function Header({ isLoggedIn }) {
+  return(
+    <div className='header'>
+      <h1>WikTok</h1>
+      {isLoggedIn ? '' : <div>Log In</div>}
     </div>
   )
 }
