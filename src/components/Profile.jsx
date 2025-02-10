2a3119026efe75f93d4997cf929bd9cfdebb2c0c
s111ew <167631312+s111ew@users.noreply.github.com>
2025-02-10 16:24:36 +0000

    Add function to remove saved article from state array

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -23,14 +23,25 @@ function Profile({ username, token }) {
         <h1>Hi, {username}.</h1>
         <p>Your Saved Articles:</p>
         </div>
-      <SavedArticles token={token} savedArticles={savedArticles} />
+      <SavedArticles token={token} savedArticles={savedArticles} setSavedArticles={setSavedArticles} />
     </div>
   )
 }
 
-function SavedArticles({ savedArticles }) {
-  const articlesToRender = savedArticles.map(article =>
-     <div key={article.title} className="card" style={ article.imageUrl ? { backgroundImage: `url(${article.imageUrl})`}  : { backgroundColor: 'black' }}>
+function SavedArticles({ token, savedArticles, setSavedArticles }) {
+  const articlesToRender = savedArticles.map((article, index) =>
+     <div key={article.title} className="card saved" style={ article.imageUrl ? { backgroundImage: `url(${article.imageUrl})`}  : { backgroundColor: 'black' }}>
+        <p className="remove-button" onClick={() => {
+          const deleteSavedArticle = async () => {
+            try {
+              await api.deleteUserSavedArticle(token, index);
+              setSavedArticles(prev => prev.filter((_, i) => i !== index))
+            } catch(err) {
+              console.error("Failed to delete article:", err)
+            }
+          }
+          deleteSavedArticle()
+        }}>X</p>
         <div className='card-text saved'>
             <h2 className="card-title saved">{article.title}</h2>
             <p className="card-description saved">{article.extract}</p>
